af64e7bb0efdf55d6f294f7dbea1140769b04450
Krishnadas <skrishnadas38@gmail.com>
2024-09-27 11:42:17 +0530

    testing 6

--- src/infrastructure/config/app.ts
+++ src/infrastructure/config/app.ts
@@ -2,11 +2,11 @@ import express from 'express'
 import cookieParser from 'cookie-parser';
 import userRoute from '../routes/userRoutes'
 import errorHandler from '../../usecase/handler/errorHandler';
-import rateLimiterMiddleware from './rateLimit';
+// import rateLimiterMiddleware from './rateLimit';
 
 export const app = express()
 
-app.use(rateLimiterMiddleware)
+// app.use(rateLimiterMiddleware)
 
 app.use(cookieParser());
 app.use(express.json({ limit: '50mb' }));
